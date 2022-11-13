bdeb668d9c1e886b3235aba5e927eaf13a318fbd
Ibrahim Ahmad <210029073@aston.ac.uk>
2022-11-13 20:15:59 +0000

    Fixed route for single product

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,6 +67,7 @@
                             @endif
                         </div>
                     </div>
+                    <br/>
                 </form>
             </div>
         </div>
